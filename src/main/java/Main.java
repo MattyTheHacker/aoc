b56ad2e38a92c6c5e519106a1e25338d62e2b720
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2023-12-02 16:05:16 +0000

    add input download functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,60 @@
-import java.net.MalformedURLException;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
+
 public class Main {
-    private static void getInput(int Day) {
+    // get cookie from .env
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String cookie = dotenv.get("SESSION");
+
+    private static void getInput(int dayString) {
         // TODO: use the day to query the website for input data for the specific day
-        String day = String.valueOf(Day);
+        String day = String.valueOf(dayString);
         try {
             URL url = new URL("https://adventofcode.com/2023/day/" + day + "/input");
-        } catch (MalformedURLException e) {
-            System.out.println(e.getMessage());
+
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Cookie", "session=" + cookie);
+
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+                String input;
+                StringBuilder sb = new StringBuilder();
+                while ((input = br.readLine()) != null) {
+                    sb.append(input);
+                    sb.append("\n");
+                }
+                sb.deleteCharAt(sb.length() - 1); // remove last newline (not needed)
+                String data = sb.toString();
+
+                // write data to file
+                String fileName = "src/main/resources/day" + dayString + ".txt";
+                try (FileOutputStream fos = new FileOutputStream(fileName)) {
+                    fos.write(data.getBytes());
+                }
+            } catch (IOException e) {
+                System.out.println("Error reading input data for day " + dayString);
+                System.out.println(con.getResponseCode() + " " + con.getResponseMessage());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
 
+    private static void getAllInputs() {
+        for (int i = 1; i <= 3; i++) {
+            getInput(i);
+        }
+    }
 
+    public static void main(String[] args) {
+        getAllInputs();
     }
 }
